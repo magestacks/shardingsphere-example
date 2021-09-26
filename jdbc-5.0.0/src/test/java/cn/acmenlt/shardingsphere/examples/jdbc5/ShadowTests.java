c7b5247dc0e3331b8998be006f136b00aec044ca
chen.ma <machencoding@163.com>
2021-09-26 22:44:01 +0800

    feat: 添加 JDBC 5.0.0 影子库示例程序.

---
+++ jdbc-5.0.0/src/test/java/cn/acmenlt/shardingsphere/examples/jdbc5/ShadowTests.java
@@ -0,0 +1,32 @@
+package cn.acmenlt.shardingsphere.examples.jdbc5;
+
+import cn.acmenlt.shardingsphere.example.core.service.ExampleService;
+import cn.acmenlt.shardingsphere.examples.jdbc5.base.BaseTest;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+
+import javax.annotation.Resource;
+
+/**
+ * Shadow tests.
+ *
+ * @author chen.ma
+ * @date 2021/9/26 21:47
+ */
+class ShadowTests extends BaseTest {
+
+    @Resource
+    private ExampleService shadowUserService;
+
+    @Test
+    @SneakyThrows
+    void contextLoads() {
+        try {
+            shadowUserService.initEnvironment();
+            shadowUserService.processSuccess();
+        } finally {
+            shadowUserService.cleanEnvironment();
+        }
+    }
+
+}
---
+++ jdbc-5.0.0/src/test/java/cn/acmenlt/shardingsphere/examples/jdbc5/base/BaseTest.java
@@ -0,0 +1,14 @@
+package cn.acmenlt.shardingsphere.examples.jdbc5.base;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base test.
+ *
+ * @author chen.ma
+ * @date 2021/9/26 21:47
+ */
+@SpringBootTest
+public class BaseTest {
+}
